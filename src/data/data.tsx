b3f98a5d4b6a49d0ea3c600e1a2a9a9ebe6224fd
MikeXXI <mike.dje@outlook.fr>
2023-04-14 11:28:21 +0200

    footer and datas update

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -44,8 +44,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'Portfolio de Mickaël ',
+  description: "portfolio de Mickaël DJEGHERIF",
 };
 
 /**
@@ -69,33 +69,35 @@ export type SectionId = typeof SectionId[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `Mickaël DJEGHERIF`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        Je suis actuellement Mentor Informatique à <strong className="text-stone-100">Sophia Ynov Campus</strong>, et suis
+        le cursus de bachelor 3 Informatique option <strong className="text-stone-100">développement</strong>.
+        Après une expérience de développeur informatique, j'ai constaté mes lacunes et mon envie de développer ce coté mangégérial. J'ai donc choisi de reprendre l'alternance.
+        Toujours à la recherche de nouvelles technologies et de nouveaux projets pour nourrir ma soif de savoir.
+          J'aime particulièrement travailler sur des projets <strong className="text-stone-100">complexes</strong> et me challengers pour sans cesse me <strong className="text-stone-100">dépasser</strong> et me <strong className="text-stone-100">perfectionner</strong>.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+      En dehors de l'informatique, je suis également un grand sportif et un passionné <strong className="text-stone-100">d'arts martiaux</strong>.
+          Je passe une grande partie de mon temps libre à pratiquer diverses activités sportives.
+          Je considère que la pratique sportive est essentielle pour maintenir un équilibre entre le <strong className="text-stone-100">corps</strong> et <strong className="text-stone-100">l'esprit</strong>, ce qui me permet de rester concentré et motivé dans mes projets.
       </p>
     </>
   ),
   actions: [
     {
-      href: '/assets/resume.pdf',
-      text: 'Resume',
-      primary: true,
-      Icon: DownloadIcon,
-    },
-    {
       href: `#${SectionId.Contact}`,
       text: 'Contact',
       primary: false,
     },
+    {
+      href: `../data/resume.pdf`,
+      text: 'Telecharger mon CV',
+      primary: true,      
+      Icon: DownloadIcon,
+    }
   ],
 };
 
@@ -104,16 +106,14 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
-  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
-  in.`,
+  description: ``,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: OfficeBuildingIcon},
+    { label: 'Ville', text: 'Mougins', Icon: MapIcon },
+    { label: 'Age', text: '31', Icon: CalendarIcon },
+    { label: 'Nationalité', text: 'Français', Icon: FlagIcon },
+    { label: 'Intérêt', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon },
+    { label: 'Etudes', text: 'Sophia Ynov Campus', Icon: AcademicCapIcon },
+    { label: 'Entreprise', text: 'Sophia Ynov Campus', Icon: OfficeBuildingIcon },
   ],
 };
 
@@ -122,69 +122,69 @@ export const aboutData: About = {
  */
 export const skills: SkillGroup[] = [
   {
-    name: 'Spoken languages',
+    name: 'Langues',
     skills: [
       {
-        name: 'English',
+        name: 'Français',
         level: 10,
       },
       {
-        name: 'French',
-        level: 4,
-      },
-      {
-        name: 'Spanish',
+        name: 'Anglais',
         level: 3,
-      },
+      }
     ],
   },
   {
-    name: 'Frontend development',
+    name: 'Développement Frontend',
     skills: [
       {
         name: 'React',
-        level: 9,
-      },
-      {
-        name: 'Typescript',
-        level: 7,
-      },
-      {
-        name: 'GraphQL',
-        level: 6,
-      },
-    ],
-  },
-  {
-    name: 'Backend development',
-    skills: [
-      {
-        name: 'Node.js',
-        level: 8,
-      },
-      {
-        name: 'Rust',
         level: 5,
       },
       {
+        name: 'Html',
+        level: 7,
+      },
+      {
+        name: 'CSS',
+        level: 6,
+      },
+      {
+        name: 'JS',
+        level: 7,
+      }
+    ],
+  },
+  {
+    name: 'Développement Backend',
+    skills: [
+      {
+        name: 'Symfony',
+        level: 5,
+      },
+      {
+        name: 'PHP',
+        level: 8,
+      },
+      {
         name: 'Golang',
-        level: 4,
+        level: 5,
       },
     ],
   },
   {
-    name: 'Mobile development',
+    name: 'Autre',
     skills: [
       {
-        name: 'React Native',
-        level: 9,
+        name: 'SQL',
+        level: 7,
       },
       {
-        name: 'Flutter',
-        level: 4,
+        name: 'API',
+        level: 6,
       },
       {
-        name: 'Swift',
+        name: 'JAVA',
         level: 3,
       },
     ],
@@ -196,69 +196,69 @@ export const skills: SkillGroup[] = [
  */
 export const portfolioItems: PortfolioItem[] = [
   {
-    title: 'Project title 1',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage1,
-  },
-  {
-    title: 'Project title 2',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    title: 'Pokedex',
+    description: 'Voici une application réalisé en React dans le cadre d\'un projet au sein de Ynov.',
+    url: 'https://pokedex-mikexxi.vercel.app/',
     image: porfolioImage2,
   },
   {
-    title: 'Project title 3',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    title: 'Funko Pop Shop',
+    description: 'Un shop réalisé en angular !',
+    url: 'https://angular-projet.vercel.app/',
+    image: porfolioImage1,
+  },
+  {
+    title: 'Mario Portfolio',
+    description: 'Voici une ébauche d\'un projet personnel réalisé en JS vanilla, dans le but de crée un mini jeux à travers un portfolio .',
+    url: 'https://js-portfolio-mikexxi.vercel.app/',
     image: porfolioImage3,
   },
   {
-    title: 'Project title 4',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    title: 'Mario Portfolio',
+    description: 'Voici une ébauche d\'un projet personnel réalisé en JS vanilla, dans le but de crée un mini jeux à travers un portfolio .',
+    url: 'https://mikexxi.me',
     image: porfolioImage4,
   },
   {
-    title: 'Project title 5',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    title: 'Mario Portfolio',
+    description: 'Voici une ébauche d\'un projet personnel réalisé en JS vanilla, dans le but de crée un mini jeux à travers un portfolio .',
+    url: 'https://mikexxi.me',
     image: porfolioImage5,
   },
   {
-    title: 'Project title 6',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    title: 'Mario Portfolio',
+    description: 'Voici une ébauche d\'un projet personnel réalisé en JS vanilla, dans le but de crée un mini jeux à travers un portfolio .',
+    url: 'https://mikexxi.me',
     image: porfolioImage6,
   },
   {
-    title: 'Project title 7',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    title: 'Mario Portfolio',
+    description: 'Voici une ébauche d\'un projet personnel réalisé en JS vanilla, dans le but de crée un mini jeux à travers un portfolio .',
+    url: 'https://mikexxi.me',
     image: porfolioImage7,
   },
   {
-    title: 'Project title 8',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    title: 'Mario Portfolio',
+    description: 'Voici une ébauche d\'un projet personnel réalisé en JS vanilla, dans le but de crée un mini jeux à travers un portfolio .',
+    url: 'https://mikexxi.me',
     image: porfolioImage8,
   },
   {
-    title: 'Project title 9',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    title: 'Mario Portfolio',
+    description: 'Voici une ébauche d\'un projet personnel réalisé en JS vanilla, dans le but de crée un mini jeux à travers un portfolio .',
+    url: 'https://mikexxi.me',
     image: porfolioImage9,
   },
   {
-    title: 'Project title 10',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    title: 'Mario Portfolio',
+    description: 'Voici une ébauche d\'un projet personnel réalisé en JS vanilla, dans le but de crée un mini jeux à travers un portfolio .',
+    url: 'https://mikexxi.me',
     image: porfolioImage10,
   },
   {
-    title: 'Project title 11',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    title: 'Mario Portfolio',
+    description: 'Voici une ébauche d\'un projet personnel réalisé en JS vanilla, dans le but de crée un mini jeux à travers un portfolio .',
+    url: 'https://mikexxi.me',
     image: porfolioImage11,
   },
 ];
@@ -268,39 +268,47 @@ export const portfolioItems: PortfolioItem[] = [
  */
 export const education: TimelineItem[] = [
   {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: '2023',
+    location: 'Sophia Ynov Campus',
+    title: 'Mastère 1 - Expert en Développement Logiciel & IoT ',
+    content: <p></p>,
   },
   {
-    date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: 'Actuellement',
+    location: 'Sophia Ynov Campus',
+    title: 'Bachelor 3 - Développement',
+    content: <p></p>,
   },
+  {
+    date: '2017',
+    location: 'UFIP',
+    title: 'Brevet de technicien supérieur - Services informatiques aux organisations',
+    content: <p></p>,
+  },
+  
 ];
 
 export const experience: TimelineItem[] = [
   {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
+    date: '2022 - Aujourd\'hui',
+    location: 'Sophia Ynov Campus',
+    title: 'Mentor informatique',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        En charge des bachelors 1 et 2 dans l'apprentissage et la compréhension des
+        langages informatiques. En charge de la correction des projets et de la
+        mise en place des projets de groupe.
       </p>
     ),
   },
   {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
+    date: '2019 - 2022',
+    location: 'DSO',
+    title: 'Développeur informatique',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        J'ai pu travailler sur plusieurs projets, l'intranet de la société conernant le materiels informatiques, sur la création d'un bot de signature ou encore
+        sur l'un des sites web pour la gestion des documents clients.
       </p>
     ),
   },
@@ -313,19 +321,19 @@ export const testimonial: TestimonialSection = {
   imageSrc: testimonialImage,
   testimonials: [
     {
-      name: 'John Doe',
-      text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
+      name: 'Socrate',
+      text: 'La chute n’est pas un échec. L’échec, c’est de rester là où on est tombé',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/b/bc/Socrate_du_Louvre.jpg',
     },
     {
-      name: 'Jane Doe',
-      text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
+      name: 'Alice Parizeau',
+      text: 'Il faut critiquer, il faut constamment tout remettre en cause. Cela permet de rester jeune et de progresser.',
+      image: 'https://img.huffingtonpost.com/asset/5ccd936823000065019b11c7.jpeg?ops=scalefit_720_noupscale&format=webp',
     },
     {
-      name: 'Someone else',
-      text: 'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
+      name: 'Boris Cyrulnik',
+      text: 'La résilience, c\'est l\'art de naviguer dans les torrents.',
+      image: 'https://images.ladepeche.fr/api/v1/images/view/5c33c7408fe56f1afb3e4e53/full/image.jpg',
     },
   ],
 };
@@ -335,28 +343,28 @@ export const testimonial: TestimonialSection = {
  */
 
 export const contact: ContactSection = {
-  headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  headerText: 'Contactez-moi',
+  description: 'L\'endroit parfait pour me joindre !',
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
+      text: 'mike.dje@outlook.fr',
+      href: 'mailto:mike.dje@outlook.fr',
     },
     {
       type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
+      text: 'Mougins',
+      href: 'https://goo.gl/maps/4iZvZDHNrJfzbsx47',
     },
     {
       type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
+      text: '@k2v2.0',
+      href: 'https://www.instagram.com/k2v2.0/',
     },
     {
       type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      text: 'mikexxi',
+      href: 'https://github.com/mikexxi',
     },
   ],
 };
@@ -365,9 +373,9 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
-  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
-  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/tbakerx/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+  { label: 'Github', Icon: GithubIcon, href: 'https://github.com/mikexxi' },
+  { label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/21632474/mikexxi' },
+  { label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/mickael-djegherif/' },
+  { label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/k2v2.0/' },
+  { label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/mikexxi' },
 ];
