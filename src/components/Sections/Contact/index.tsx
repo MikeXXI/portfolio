95f1fa17e78fdb26c4b1c6c41f478c327c45bcc8
MikeXXI <mickael.djegherif@outlook.fr>
2024-06-28 01:31:41 +0200

    Update Contact

--- src/components/Sections/Contact/index.tsx
+++ src/components/Sections/Contact/index.tsx
@@ -29,8 +29,7 @@ const Contact: FC = memo(() => {
     <Section className="bg-neutral-800" sectionId={SectionId.Contact}>
       <div className="flex flex-col gap-y-6">
         <div className="flex flex-col gap-6 md:flex-row md:items-center">
-          {/* <MapPinIcon className="hidden h-16 w-16 text-white md:block" /> */}
-          <Image src="/images/contact-me.webp" alt="Map Pin" width={64} height={64} className="hidden h-16 w-16 text-white md:block" />
+          <MapPinIcon className="hidden h-16 w-16 text-white md:block" />
           <h2 className="text-2xl font-bold text-white">{headerText}</h2>
         </div>
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
